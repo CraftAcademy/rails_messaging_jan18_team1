24f71a7fc2664655a4fad9f6fb8e8ddeca5dc17d
vick3d <viktoro0090@gmail.com>
2019-02-17 21:24:40 +0100

    adds test for receiving email. adjusts order of test steps

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,48 +1,52 @@
   Given("following users exist") do |table|
-		table.hashes.each do |user|
-			FactoryBot.create(:user, user)
-		end
-	end
+    table.hashes.each do |user|
+      FactoryBot.create(:user, user)
+    end
+  end
 
-	And ("I visit the site") do
-		visit root_path
-	end
+  And ("I visit the site") do
+    visit root_path
+  end
 
-	When("I fill in {string} with {string}") do |field, input|
-		fill_in field, with: input
-	end
+  When("I fill in {string} with {string}") do |field, input|
+    fill_in field, with: input
+  end
 
-	When("I click on {string}") do |button|
-		click_on button
-	end
+  When("I click on {string}") do |button|
+    click_on button
+  end
+  
 
-	Given("I am logged in as {string}") do |name|
-		user = User.find_by(name: name)
-		login_as user, scope: :user
-	end
+  Given("I am logged in as {string}") do |name|
+    user = User.find_by(name: name)
+    login_as user, scope: :user
+  end
+  
+  
+  When("select {string} as {string}") do |names, string2|
+    select names, from: string2
+  end
+  
+  Given("I am visiting the {string}") do |path|
+    case path
+    when "Inbox"
+      visit mailbox_inbox_path
+    else 
+      return false
+    end
+  end
 
-	Given("I am visiting the {string}") do |path|
-		case path
-		when "Inbox"
-			visit mailbox_inbox_path
-		else
-			return false
-		end
-	end
-
-	When("select {string} as {string}") do |names, string2|
-		select names, from: string2
-	end
-	  
 	Given("the following email exists") do |table|
-    	table.hashes.each do |email|
-        	sender = User.find_by(name: email[:sender])
-        	@receiver = User.find_by(name: email[:receiver])
-        	sender.send_message(@receiver, email[:body], email[:subject])
-    	end
-	end
-
-	
-
-	
-	
+    table.hashes.each do |email|
+        sender = User.find_by(name: email[:sender])
+        @receiver = User.find_by(name: email[:receiver])
+        sender.send_message(@receiver, email[:body], email[:subject])
+    end
+  end 
+    
+  Then("my message should open") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  
+ 
--- features/user_can_receive_mail.feature
+++ features/user_can_receive_mail.feature
@@ -1,21 +1,23 @@
 Feature: User is able to receive mail
+    As a User
+	In order to let other people communicate with me
+	I would like to be able to receive emails.
 
-Background:
-        Given I visit the site
-        And following users exist
-|   name        |   email                       |   password    |   password_confirmation    |
-|   User        |   user@yahoo.com              |   11111111    |   11111111                 |
-|   Nici        |   niciputter@yahoo.com        |   11111111    |   11111111                 |
-        And I am logged in as "User"
-        And I am visiting the "Inbox"
 
-	And the following email exists
-| sender  	| receiver | body          | subject       |
-| User	        | Nici     | some message  | some subject  |
+	Background:
+		Given following users exist
+			|       name            |   email                       |   password    |   password_confirmation    |
+			|       Viktor          |   user@yahoo.com              |   11111111    |   11111111                 |
+			|       Nici            |   niciputter@yahoo.com        |   11111111    |   11111111                 |
+		And the following email exists
+			|       sender  	| receiver      | body          | subject       |
+			|       Nici	    | Viktor        | some message  | some subject  |
+		And I am logged in as "Viktor"
+		And I am visiting the "Inbox"
 
-Scenario: User can receive mail
-	When I click  "View" 
-	Then my message should open
+	Scenario: User can receive mail
+		When I click on "View" 
+		Then I should see "Move to trash"
         
 #Scenario: User can reply to mail
 	#When I type in a message
